4b86401cedec86170a8619795015359be3389864
getlarge <ed@getlarge.eu>
2021-06-11 12:58:01 +0200

    override Home and Navbar components

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,12 +2,30 @@ const pkg = require('../../package.json');
 
 module.exports = {
   title: 'S1Seven Developers guide',
-  base: `/${pkg.name}/`,
+  // base: `/${pkg.name}/`,
+  base: `/${pkg.name}${process.env.notifyBCDocVersion_PATH || '/'}`,
+  description: pkg.description,
+  head: [
+    ['meta', {name: 'theme-color', content: '#3eaf7c'}],
+    ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
+    ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
+    ['link', {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+      },
+    ],
+  ],
   themeConfig: {
     logo: '/logo.png',
     repo: pkg.repository.url,
     repoLabel: 'Git',
     docsDir: 'docs',
+    editLinks: false,
+    editLinkText: '',
+    lastUpdated: false,
     nav: [{ text: 'Home', link: '/' }],
     sidebar: [
       ['/informations/', 'General API information'],
